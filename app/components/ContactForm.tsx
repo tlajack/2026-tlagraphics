a60ba8f22944de8ddb151ac4bcef47c1554df081
tlaJack <jack@tlagraphics.com>
2026-02-12 17:22:32 -0500

    update to form

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -20,7 +20,7 @@ export default function ContactForm() {
     });
 
     try {
-      const response = await fetch("/", {
+      const response = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: params.toString(),
