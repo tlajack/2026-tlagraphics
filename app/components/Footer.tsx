b0dcd9af7d8937e2939046eca933efc5caf661aa
tlaJack <jack@tlagraphics.com>
2026-04-09 16:41:58 -0400

    remove emaillink component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import EmailLink from "./EmailLink";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
@@ -18,7 +17,9 @@ export default function Footer() {
             +1 (905) 726-2600
           </a>
           <br />
-          <EmailLink email="info@tlagraphics.com" className={styles.link} />
+          <a href="mailto:info@tlagraphics.com" className={styles.link}>
+            info@tlagraphics.com
+          </a>
           <br />
           <a
             href="https://www.dropbox.com/request/FL93CCTD7UCysvFI2Pp6"
